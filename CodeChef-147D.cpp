66a7e8dba236c22944474c53a6bff4fccd54d79e
Rachit-Dhaka <rachitdhaka29@gmail.com>
2024-08-14 21:40:36 +0530

    Q3

--- CodeChef-147D.cpp
+++ CodeChef-147D.cpp
@@ -21,30 +21,59 @@
 
 // Question 2 
 
+// #include <bits/stdc++.h>
+// #include <algorithm>
+// using namespace std;
+
+// int main() {
+// 	// your code goes here
+// 	int t;
+// 	cin>>t;
+	
+//     while(t--){
+//         int a,b,c;
+//         cin>>a>>b>>c;
+        
+//         int m=max({a,b,c});
+//         int sum=a+b+c;
+//         int result=(sum-m)+1;
+        
+//         if(m<=result){
+//             cout<< "YES"<<endl;
+//         }
+//         else{
+//             cout<<"NO"<<endl;
+//         }
+        
+//     }
+
+// }
+
+
+// Question 3
 #include <bits/stdc++.h>
-#include <algorithm>
 using namespace std;
 
 int main() {
 	// your code goes here
+	
 	int t;
 	cin>>t;
-	
-    while(t--){
-        int a,b,c;
-        cin>>a>>b>>c;
-        
-        int m=max({a,b,c});
-        int sum=a+b+c;
-        int result=(sum-m)+1;
-        
-        if(m<=result){
-            cout<< "YES"<<endl;
-        }
-        else{
-            cout<<"NO"<<endl;
-        }
-        
-    }
+	while(t--){
+	    int n,m;
+	    cin>>n>>m;
+	    int tot=(m*2)+1;
+	    if(m>=n){
+	        cout<<-1<<endl;
+	        
+	    }
+	    else{
+	        cout<<tot<<endl;
+	        
+	    }
+	    
+	}
 
 }
+}
+
